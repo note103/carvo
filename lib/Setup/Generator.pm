4fe4ee3b4a96e03b6b5dc1b9fbad112d33a57f51
Hiroaki Kadomatsu <note103@gmail.com>
2016-02-04 14:10:01 +0900

    Change code to use module, better variables, croak messages

--- lib/Setup/Generator.pm
+++ lib/Setup/Generator.pm
@@ -8,44 +8,41 @@ package Generator {
         use JSON;
         use Encode;
         use open qw/:utf8 :std/;
+        use Path::Tiny;
 
-        my ($fh, $file, $card);
-        my ($lesson, $fmt, $card_head_in, $card_dir, $card_body) = @_;
+        my ($fh, $dict, $card);
+        my ($lesson, $fmt, $card_head, $card_dir, $card_name) = @_;
 
-        my $card_filename;
-        opendir(my $dh, $card_dir) or croak("Can't open file.");
-        for $file (readdir $dh) {
+        my $card_iter = path($card_dir)->iterator;
+        while (my $file = $card_iter->()) {
             if ($file =~ /\.$fmt$/) {
-                $card = "$card_dir/" . "$file";
+                $dict = $file;
             }
-            elsif ($file =~ /^$card_head_in\D*.*txt$/) {
-                $card_filename = "$card_dir/" . "$file";
+            elsif ($file =~ /$card_head\_.+\.txt$/) {
+                $card = $file;
             }
         }
-        closedir $dh;
 
-        my $dict;
         if ($fmt eq 'yml') {
-            $dict = YAML::LoadFile($card);
+            $dict = YAML::LoadFile($dict);
         }
         else {
-            open my $fh, '<', $card or croak("Can't open file.");
+            open my $fh, '<', $dict or croak("Can't open JSON file.");
             my $json = do { local $/; <$fh> };
             $dict = decode_json(encode('utf8', $json));
             close $fh;
         }
 
-        open $fh, '<', $card_filename or croak("Can't open file.");
-        my @card_names = <$fh>;
-        close $fh;
+        open $fh, '<', $card or croak("Can't open card file.");
         my %set_dict;
-        for (@card_names) {
+        for (<$fh>) {
             chomp;
             $set_dict{$_} = $dict->{$_};
         }
         my $set_dict = \%set_dict;
+        close $fh;
 
-        return ($set_dict, $fmt, $card_body);
+        return ($set_dict, $fmt, $card_name);
     }
 }
 1;
